b5a23fb134743fd6e8ef142b261b42649e9b408e
beland.dev <>
2022-03-04 20:02:38 +0700

    fix

--- subgraphs/land/mappings/land-bundle.ts
+++ subgraphs/land/mappings/land-bundle.ts
@@ -7,6 +7,7 @@ export function handleTransfer(event: Transfer): void {
   if (!estate) {
     estate = new Estate(event.params.tokenId.toString());
     estate.parcels = [];
+    estate.size = 0;
   }
   estate.owner = event.params.to;
   estate.save();
@@ -14,43 +15,50 @@ export function handleTransfer(event: Transfer): void {
 
 export function handleCreateBundle(event: BundleNew): void {
   let estate = Estate.load(event.params.tokenId.toString());
-  for (let i = 0; i < event.params.tokenIds.length; i++) {
-    let parcel = Parcel.load(event.params.tokenIds[i].toString());
+  let tokenIds = event.params.tokenIds;
+
+  for (let i = 0; i < tokenIds.length; i++) {
+    let parcel = Parcel.load(tokenIds[i].toString());
     parcel.estate = estate.id;
     parcel.save();
-    estate.parcels.push(parcel.id);
   }
+  estate.parcels = tokenIds.map<string>((tokenId) => tokenId.toString());
+  estate.size = estate.parcels.length;
   estate.save();
 }
 
 export function handleBundleAddItems(event: BundleAdd): void {
   let estate = Estate.load(event.params.tokenId.toString());
-  for (let i = 0; i < event.params.tokenIds.length; i++) {
-    let parcel = Parcel.load(event.params.tokenIds[i].toString());
+  let tokenIds = event.params.tokenIds;
+  for (let i = 0; i < tokenIds.length; i++) {
+    let parcel = Parcel.load(tokenIds[i].toString());
     parcel.estate = estate.id;
     parcel.save();
-    estate.parcels.push(parcel.id);
   }
+  estate.parcels = estate.parcels.concat(tokenIds.map<string>((tokenId) => tokenId.toString()));
+  estate.size = estate.parcels.length;
   estate.save();
 }
 
 export function handleBundleRemoveItems(event: BundleRemove): void {
   let estate = Estate.load(event.params.tokenId.toString());
-  for (let i = 0; i < event.params.tokenIds.length; i++) {
-    var parcelId = event.params.tokenIds[i].toString();
+  let tokenIds = event.params.tokenIds;
+  for (let i = 0; i < tokenIds.length; i++) {
+    var parcelId = tokenIds.toString();
     let parcel = Parcel.load(parcelId);
     parcel.estate = null;
     parcel.save();
     estate.parcels.splice(estate.parcels.indexOf(parcelId), 1);
   }
+  estate.size = estate.parcels.length;
   estate.save();
 }
 
-export function handleUpdateMetadata(event: MetadataUpdate) {
+export function handleUpdateMetadata(event: MetadataUpdate): void {
   let estate = Estate.load(event.params.tokenId.toString());
   let data = event.params.data.toString();
   estate.rawData = data;
-  let estateDataId = DataType.ESTATE + "-" + estate.id;
+  let estateDataId = "ESTATE-" + estate.id;
   let estateData = buildData(estateDataId, data, DataType.ESTATE);
   if (estateData != null) {
     estate.data = estateDataId;
--- subgraphs/land/mappings/land.ts
+++ subgraphs/land/mappings/land.ts
@@ -4,25 +4,25 @@ import { MetadataUpdate } from "../generated/LandBundle/LandBundle";
 import { Parcel } from "../generated/schema";
 import { buildData, DataType } from "./data";
 
-const WIDTH = BigInt.fromI32(300);
-const HEIGHT = BigInt.fromI32(300);
+let WIDTH = BigInt.fromI32(300);
+let HEIGHT = BigInt.fromI32(300);
 
 export function handleTransfer(event: Transfer): void {
   let parcel = Parcel.load(event.params.tokenId.toString());
   if (!parcel) {
     parcel = new Parcel(event.params.tokenId.toString());
-    parcel.x = event.params.tokenId.div(WIDTH);
-    parcel.y = event.params.tokenId.mod(HEIGHT);
+    parcel.x = event.params.tokenId.mod(WIDTH);
+    parcel.y = event.params.tokenId.div(HEIGHT);
   }
   parcel.owner = event.params.to;
   parcel.save();
 }
 
-export function handleUpdateMetadata(event: MetadataUpdate) {
+export function handleUpdateMetadata(event: MetadataUpdate): void {
   let parcel = Parcel.load(event.params.tokenId.toString());
   let data = event.params.data.toString();
   parcel.rawData = data;
-  let parcelDataId = DataType.PARCEL + "-" + parcel.id;
+  let parcelDataId = "PARCEL-" + parcel.id;
   let parcelData = buildData(parcelDataId, data, DataType.PARCEL);
   if (parcelData != null) {
     parcel.data = parcelDataId;
